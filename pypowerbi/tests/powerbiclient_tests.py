f70bc54bf225ee1d5715b174aa70512538f52c5d
Chris Berry <chris@chrisberry.com.au>
2018-08-28 12:28:35 +1000

    added groups tests

--- pypowerbi/tests/powerbiclient_tests.py
+++ pypowerbi/tests/powerbiclient_tests.py
@@ -584,23 +584,17 @@ class PowerBIAPITests(TestCase):
         client.datasets.delete_dataset(dataset.id, group_id)
 
     def test_groups_count_method(self):
-        for group_id in self.group_ids:
-            self._test_groups_count_method(self.client, group_id)
-
-    def _test_groups_count_method(self, client, group_id):
-        group_count = client.groups.count()
-        self.assertGreater(0, group_count)
+        group_count = self.client.groups.count()
+        self.assertGreater(group_count, 0)
 
     def test_groups_has_group_method(self):
         for group_id in self.group_ids:
             self._test_groups_has_group_method(self.client, group_id)
 
     def _test_groups_has_group_method(self, client, group_id):
-        pass
+        self.assertTrue(client.groups.has_group(group_id))
 
     def test_groups_get_groups_method(self):
-        for group_id in self.group_ids:
-            self._test_groups_get_groups_method(self.client, group_id)
-
-    def _test_groups_get_groups_method(self, client, group_id):
-        pass
+        groups = self.client.groups.get_groups()
+        for group in groups:
+            self.assertTrue(self.client.groups.has_group(group.id))
