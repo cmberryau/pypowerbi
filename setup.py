fd929cb2b06323a53949a534c6f2f6f4d9cf0276
Chris Berry <chris@chrisberry.com.au>
2018-08-28 12:29:25 +1000

    updated version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pypowerbi',
-      version='0.21',
+      version='0.22',
       description='A python library for Microsoft\'s PowerBI',
       url='http://github.com/cmberryau/pypowerbi',
       author='Chris Berry',
