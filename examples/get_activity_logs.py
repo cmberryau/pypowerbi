90d9fa27d543a2c90a612d7a4af1ebd3bf690895
Andrew Dahlin <adahlin@mckinleycapital.com>
2020-01-13 10:28:02 -0900

    Added optional pandas processing. Renamed file for consistency.

--- examples/get_activity_logs.py
+++ examples/get_activity_logs.py
@@ -12,12 +12,21 @@ client = PowerBIClient.get_client_with_username_password(client_id=client_id, us
 # When testing, only logs from December 15th, 2019 and later were available. This may change in the future though.
 dt = datetime(2019, 12, 16)
 logs = client.activity_logs.get_activity_logs(dt)
-
-df = pd.DataFrame(logs)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
-# print(df["UserId"].unique())
-#print(df.columns)
-#df.to_csv(r"d:\powerbi_activity.csv")
-
 print(logs)
+
+pandas_installed = True
+try:
+    import pandas as pd
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
+except ImportError:
+    pandas_installed = False
+
+if pandas_installed:
+    # If pandas is installed, we can do lots of neat analysis
+    df = pd.DataFrame(logs)
+    print(df.columns)
+    print(df["UserId"].unique())
+    #df.to_csv(r"d:\powerbi_activity.csv")
+
+
