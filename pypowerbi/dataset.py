3175cf9a718b142e9788f695a0538544cc9f51c0
Valeriy Kondratenko <kondratenko.valeriy@gmail.com>
2020-02-28 11:21:29 +0300

    Column format string: Make formatstring optional

--- pypowerbi/dataset.py
+++ pypowerbi/dataset.py
@@ -234,7 +234,7 @@ class Column:
     datatype_key = 'dataType'
     formatstring_key = 'formatString'
 
-    def __init__(self, name, data_type, formatstring):
+    def __init__(self, name, data_type, formatstring=None):
         self.name = name
         self.data_type = data_type
         self.formatstring = formatstring
@@ -245,11 +245,15 @@ class Column:
 
 class ColumnEncoder(json.JSONEncoder):
     def default(self, o):
-        return {
+        json_dict = {
             Column.name_key: o.name,
-            Column.datatype_key: o.data_type,
-            Column.formatstring_key: o.formatstring
+            Column.datatype_key: o.data_type
         }
+
+        if o.formatstring is not None:
+            json_dict[Column.formatstring_key] = o.formatstring
+
+        return json_dict
 
 
 class Row:
