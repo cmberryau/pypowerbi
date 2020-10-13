a97a783fab05d74988b0fc2ab79c4c76fbd47206
Chris Berry <chris.berry@lodlaw.com>
2020-10-13 16:38:24 +1100

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pypowerbi',
-      version='0.25',
+      version='0.26',
       description='A python library for Microsoft\'s PowerBI',
       url='http://github.com/cmberryau/pypowerbi',
       author='Chris Berry',
