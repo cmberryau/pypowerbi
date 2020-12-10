226c685401cd84468e095a967e162f3548e19561
Ben van der Harg <b.vanderharg@gsstatistics.com>
2020-12-10 11:22:22 +0100

    Generalize deserialization of get_multiple operations responses

--- pypowerbi/gateways.py
+++ pypowerbi/gateways.py
@@ -39,11 +39,7 @@ class Gateways:
         if response.status_code != 200:
             raise HTTPError(response, f'Get Gateways request returned http error: {response.json()}')
 
-        return self.gateways_from_get_gateways_response(response)
-
-    @classmethod
-    def gateways_from_get_gateways_response(cls, response):
-        """Creates a list of gateways from a http response object
+        return self._models_from_get_multiple_response(response, Gateway)
 
     def get_datasources(self, gateway_id: str) -> List[GatewayDatasource]:
         """Returns a list of datasources from the specified gateway
@@ -66,24 +62,30 @@ class Gateways:
         if response.status_code != 200:
             raise HTTPError(response, f'Get Gateway Datasources request returned http error: {response.json()}')
 
-        return self.datasources_from_get_datasources_response(response)
+        return self._models_from_get_multiple_response(response, GatewayDatasource)
 
     @classmethod
-    def datasources_from_get_datasources_response(cls, response):
-        """Creates a list of gateway datasources from a http response object
+    def _models_from_get_multiple_response(
+            cls,
+            response: requests.Response,
+            model_class: Type[Deserializable]
+    ) -> List[Any]:
+        """Creates a list of models from a http response object
 
         :param response:
             The http response object
+        :param model_class:
+            The model to transform the response items into
         :return: list
-            The list of gateway datasources
+            The list of model_class instances
         """
 
         # parse json response into a dict
         response_dict = json.loads(response.text)
 
         # Add parsed Gateway objects to list
-        gateway_datasources = []
+        items = []
         for entry in response_dict[cls.odata_response_wrapper_key]:
-            gateway_datasources.append(GatewayDatasource.from_dict(entry))
+            items.append(model_class.from_dict(entry))
 
-        return gateway_datasources
+        return items
